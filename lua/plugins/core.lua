d1943a108b4d4e5c888bac4ad66d7d13e98686a0
Mark A. <mark001616@gmail.com>
2023-11-15 19:31:36 -0500

    Move colorscheme to a file

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,13 +2,6 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        require("catppuccin").load()
-      end,
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-      }),
       background_colour = "#000000",
     },
   },
