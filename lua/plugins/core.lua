d09cc058e856b40898536ce642e815eb992b1841
Mark A. <mark001616@gmail.com>
2023-11-15 19:48:57 -0500

    Remove notification warning

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -3,6 +3,9 @@ return {
     "LazyVim/LazyVim",
     opts = {
       background_colour = "#000000",
+      require("notify").setup({
+        background_colour = "#000000",
+      }),
     },
   },
 }
