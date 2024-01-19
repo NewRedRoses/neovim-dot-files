c04e3d1c493a98a12a3793a0aa573244a2f96f37
Mark A. <mark001616@gmail.com>
2024-01-18 21:24:22 -0500

    Properly load filetype settings with lazyvim

--- lua/plugins/codeRunner.lua
+++ lua/plugins/codeRunner.lua
@@ -2,20 +2,21 @@ return {
   {
     "CRAG666/code_runner.nvim",
     config = true,
-    filetype = {
-      java = {
-        "cd $dir &&",
-        "javac $fileName &&",
-        "java $fileNameWithoutExt",
-      },
-      python = "python3 -u",
-      typescript = "deno run",
-      rust = {
-        "cd $dir &&",
-        "rustc $fileName &&",
-        "$dir/$fileNameWithoutExt",
+    opts = {
+      filetype = {
+        java = {
+          "cd $dir &&",
+          "javac $fileName &&",
+          "java $fileNameWithoutExt",
+        },
+        python = "python3 -u",
+        typescript = "deno run",
+        rust = {
+          "cd $dir &&",
+          "rustc $fileName &&",
+          "$dir/$fileNameWithoutExt",
+        },
       },
     },
-    mode = "float",
   },
 }
